cf5dcf073dd6ee1195b6f4814c651392fb350557
Jason_Naicker <jasonnaickerother@gmail.com>
2026-07-21 02:13:48 -0500

    Skeleton

--- Encoder.py
+++ Encoder.py
@@ -1,13 +1,23 @@
 from typing import Optional, Final, TypeAlias, Generic
 
 QrInput : TypeAlias = str
-
+QrMatrix : TypeAlias = list[list[int]]
 class QrEncoder():
-    __slots__ = ("input")
-    def __init__(self, input : QrInput) -> None:
-        self.input = input
-    
-    def encode(self) -> str:
+    __slots__ = ("data")
+    def __init__(self, data : QrInput) -> None:
+        self.data = data
+
+    def _encode_data() -> None:
+        pass
+
+    def _error_correction() -> None:
+        pass
+
+    def _place_modules() -> None:
+        pass
+
+    def _apply_mask() -> None:
         pass
     
-    
+    def encode(self) -> QrMatrix:
+        pass
